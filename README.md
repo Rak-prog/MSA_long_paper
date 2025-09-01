78ad8aa2c8683e675a9c705b74daec6b912e2816
Rak-prog <riccardo.iandolo@gmail.com>
2025-09-01 15:59:21 +0200

    add script for metric extraction, visualize and check fs results and run post freesurfer segmentation

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Code used for the MSA paper.
 
 dicom2bids.py and do_stats.py convert dicom to bids format, process dicom information and create information for descriptive statistics
 
+bash script are to run freesurer, visualize results and run post fs substructure segmentation 
+
 final_paper.m mani script to perform the lme modeling with accessory functions (the others .m files)
 
 figure_paper.ipynb generate final figures for the papers (many more figures are shown to ivnestigate the results properly) 
--- substructure_segmentation.sh
+++ substructure_segmentation.sh
@@ -9,16 +9,16 @@ for structure in brainstem; # {thalamus,hippo-amygdala}
 do
     for folder in ${subject_list};
     do 
-        #if [[ ! "$folder" == *"long"* ]];then # exluding the longitudinal subjects, get only the base_
+        if [[ ! "$folder" == *"long"* ]];then # exluding the longitudinal subjects, get only the base_
             #echo $folder
             #
-            #echo $subj_id
-            #segment_subregions ${structure} --long-base ${subj_id} --debug --threads ${nthreads}  # IF LONGITUDINAL DATA USE --long-base
-            #segment_subregions ${structure} --cross ${subj_id} --debug --threads ${nthreads} # IF cross I ran this for the controls
-        if [[ ! "$folder" == *"sub-HC"* ]];then  # for the cross study get the patients (the HC have been already done) 
-            subj_id=$(basename $folder)
             echo $subj_id
-            segment_subregions ${structure} --cross ${subj_id} --debug --threads ${nthreads} # run cross forthe patient sonly cross sectional (t0 all subejcts indluing also the one without the FUP)
+            segment_subregions ${structure} --long-base ${subj_id} --debug --threads ${nthreads}  # IF LONGITUDINAL DATA USE --long-base
+            segment_subregions ${structure} --cross ${subj_id} --debug --threads ${nthreads} # IF cross I ran this for the controls
+        #if [[ ! "$folder" == *"sub-HC"* ]];then  # for the cross study get the patients (the HC have been already done) 
+        #    subj_id=$(basename $folder)
+        #    echo $subj_id
+        #    segment_subregions ${structure} --cross ${subj_id} --debug --threads ${nthreads} # run cross forthe patient sonly cross sectional (t0 all subejcts indluing also the one without the FUP)
         fi 
 
     done 
